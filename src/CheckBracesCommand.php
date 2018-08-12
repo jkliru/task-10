03a8be66adde1f97f2c4ed0e17a9ef6d21acdb89
S.Cheglakov <loxmatwj@yandex.ru>
2018-08-12 10:24:56 +0300

    Use jkliru/check-braces library in console application

---
+++ src/CheckBracesCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace jkliru;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use CheckBraces\CheckBraces;
+
+class CheckBracesCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('sum')
+            ->setDescription('This command checks string for braces')
+            ->addArgument('string', InputArgument::REQUIRED, 'String');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $string = $input->getArgument('string');
+
+        $checkBraces = new CheckBraces();
+
+        try {
+            $stringIsCorrect = $checkBraces->isCorrectString($string);
+
+            $output->writeln("string '$string' is " . ($stringIsCorrect ? 'correct' : 'NOT correct'));
+        }
+        catch (\Exception $e) {
+            $output->writeln("string '$string' contains illegal characters");
+        }
+    }
+}
